af9d14136037363dcbfee3a5f5e54b9a1f6fa515
Semyon Yakimov <sdyakimov@gmail.com>
2019-10-21 19:52:22 +0300

    Results urls from json

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 import requests
+import json
 from sfr_html_parser import SfrHtmlParser
 from event_rating import EventRating
 
 def main():
-    #url = 'http://rogaining.msk.ru/protokol/Result_Tainstvenniy-Les_2019.htm'
-    #url = 'http://rogaining.msk.ru/protokol/Result_Pozdnaya_Osen_2019.htm'
-    #url = 'http://rogaining.msk.ru/protokol/Result_MosDen_2019.htm'
-    url = 'http://rogaining.msk.ru/protokol/Result-Zimniy-Rogaining-2019.htm'
-    r = requests.get(url)
-    r.raise_for_status()
+    with open('urls.json') as urls_json_file:
+        rogaines = json.load(urls_json_file)
+        for rogaine in rogaines:
+            print(rogaine['name'])
+            r = requests.get(rogaine['url'])
+            r.raise_for_status()
 
-    sfr_html_parser = SfrHtmlParser()
-    participants = sfr_html_parser.parse_results(r.text)
-    event_rating = EventRating(participants)
-    event_rating.print()
+            sfr_html_parser = SfrHtmlParser()
+            r.encoding = 'cp1251'
+            participants = sfr_html_parser.parse_results(r.text)
+            event_rating = EventRating(participants)
+            #event_rating.print()
 
 if __name__ == '__main__':
     main()
